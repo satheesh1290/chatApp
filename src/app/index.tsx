979cbd48e92134cc9af8ac6910751bdd7dc05908
sonarathinam <sonarathina22@gmail.com>
2024-07-27 16:48:33 +0530

    added eas build

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,4 @@
 import { Redirect } from "expo-router";
-import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
-import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
-import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
-
-polyfillReadableStream();
-polyfillEncoding();
-polyfillFetch();
 
 export default function HomeScreen() {
   return <Redirect href={"/(auth)/login"} />;
